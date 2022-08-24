7c13de62c3b9f51c87d613439b5bc4742333d30a
tania <taniaosano15@gmail.com>
2022-08-24 14:33:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(name){
+    let card = [];
+    for (let i=0; i < name.length; i++){
+        card.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        
+    }
+    return card;
+}
+function countDown(y){
+    
+    while (y > 0){
+        console.log(y);
+    y--;
+}
+    console.log(y);
+}
